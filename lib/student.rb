c32f05c2dbe3a0ead81e8ccdf22ce0040e9610ee
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-21 22:52:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
 class Student < User 
+  def initialize(ind_students,@knowledge)
+    @ind_students=ind_students
+    @knowledge =[]
+  end 
+  def learn ("")
+    @knowledge << learn 
+  end 
+  def knowledge
+    student_know = []
+  end 
 
 end
